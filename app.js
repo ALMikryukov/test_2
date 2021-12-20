4e6b8deb96c56acc1653f96234744f58d8698c35
Alex <example@gmail.com>
2021-12-20 09:43:00 +0500

    new commit

---
+++ app.js
@@ -0,0 +1,37 @@
+
+// // const a= 15;
+
+// // const obj = {
+// //     name: "Jonh",
+// //     age: 25,
+// //     isMarried: false
+// // }
+// // obj.age = 30;
+// // console.log(obj["name"]);
+// // console.log(obj.age );
+
+// const result  = confirm("Are you still here??","");
+
+// const answers  = []; 
+// answers[0] = prompt ("Вам уже есть  18 ?","");
+// answers[1] = prompt ("Ваша фамилия ?","");
+// answers[2] = prompt ("Ваше имя ?","");
+
+// document.write(answers);
+// const category = 'toys';
+
+// // const user ='Ivan';
+// const user ='Ivan';
+// // alert(`Привет, ${user}`);
+
+// let incr = 10,
+//   dcr = 10;
+  
+//   console.log(++incr ,-- dcr );
+const  isChecked = true,
+isClosed = true;
+console.log(isChecked || !isClosed);
+
+
+
+
